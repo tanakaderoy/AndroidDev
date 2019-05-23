d47143494bbdb2db61090ae877cc4b60701b1a81
Tanaka Mazivanhanga <42681052+tanakaderoy@users.noreply.github.com>
2019-05-22 23:03:47 -0400

    Projects

---
+++ HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.helloworld
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Log.d("Main Activity", "Hello World");
+
+    }
+}
